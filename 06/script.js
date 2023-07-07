de68f3a797952d04fb6ebae2eaf5fc94c0fb52e8
vishwa shah <9vishwashah@gmail.com>
2023-07-07 17:50:25 +0530

    update

---
+++ 06/script.js
@@ -0,0 +1,73 @@
+// function isPositive(number) {
+//     return number >= 0
+// }
+
+// console.log(isPositive(-9));
+
+// function randomNumber(){
+//    return Math.random;
+// }
+
+// console.log(randomNumber());
+
+// document.addEventListener('click', function(){
+//     console.log('hey');
+// })
+
+// document.addEventListener('click', function(){
+//     document.body.style.backgroundColor = "red";
+//     document.body.style.backgroundColor = "blue";
+//     document.body.style.backgroundColor = "yellow";
+
+// })
+
+// const btn = document.getElementById('btn');
+
+// btn.addEventListener('click', function onClick(event){
+//     event.target.style.backgroundColor = 'salmon';
+// });
+
+// var colors = ["red", "blue", "green", "yellow"],   // the color choices
+//     index = 0;                                     // index of the current color
+    
+// document.getElementById("colorify").onclick = function() {
+//   document.body.style.background = colors[index];  // set the color of body to the current color
+//   index = (index + 1) % colors.length;             // increment index to point to the next color (if it goes beyond the length of the coices array get it back to 0 using the modulo %)
+// }
+
+// const changeColor = document.getElementById('changeColor'),
+//       colors      = ['red', 'green', 'blue'];
+// let   colorIndex  = 0;
+
+// changeColor.addEventListener('click', () => {
+//   document.body.style.backgroundColor = colors[colorIndex];      
+//   colorIndex = (colorIndex + 1) % colors.length
+// });
+
+// let sum6 = (a, b) => a+b;
+// console.log(sum6(2,4));
+
+// document.addEventListener('click', () => {
+//     document.body.style.background = 'red';
+// })
+
+
+
+// document.addEventListener('click', ()=> console.log('grshgoi'))
+
+class Person{
+    constructor(name){
+        this.name= name;
+    }
+
+    printNameArrow(){
+        setTimeout(()=>{
+            console.log('Arrow:'+this.name);
+        }, 10000)
+
+    }
+
+}
+
+let sum6 = new Person('bob')
+sum6.printNameArrow()

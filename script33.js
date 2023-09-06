ee77f64c12cda6f8df0b664934188d618bcf4983
vishwa shah <9vishwashah@gmail.com>
2023-09-06 17:39:55 +0530

    completed update

--- script33.js
+++ script33.js
@@ -30,12 +30,60 @@ function checkPalindrome(str) {
     }
 
 }
-console.log(checkPalindrome("level"));
+// console.log(checkPalindrome("level"));
+
+// var students = [['Summit', 80], ['Kalpesh', 77], ['Amit', 88], ['Tejas', 93],
+// ['Abhishek', 65]];
+//  var Avgmarks = 0;
+//  for (var i=0; i < students.length; i++) {
+//  Avgmarks += students[i][1];
+//  }
+//  var avg = (Avgmarks/students.length);
+//  document.write("Average grade: " + (Avgmarks)/students.length);
+//  document.write("<br>");
+//  if (avg < 60){
+// document.write("Grade : E");
+//  }
+//  else if (avg < 70) {
+//  document.write("Grade : D");
+//  }
+//  else if (avg < 80) {
+//  document.write("Grade : C");
+//  } else if (avg < 90) {
+//  document.write("Grade : B");
+//  } else if (avg < 100) {
+//  document.write("Grade : A");
+//  }
 
 
-
-
-
+// function calculateAverage(marks) {
+//     const totalMarks = marks.reduce((sum, mark) => sum + mark, 0);
+//     return totalMarks / marks.length;
+//   }
+  
+//   function getGrade(average) {
+//     if (average >= 90) {
+//       return "A";
+//     } else if (average >= 80) {
+//       return "B";
+//     } else if (average >= 70) {
+//       return "C";
+//     } else if (average >= 60) {
+//       return "D";
+//     } else {
+//       return "F";
+//     }
+//   }
+  
+//   // Example student marks
+//   const studentMarks = [85, 92, 78, 60, 45];
+  
+//   const averageMarks = calculateAverage(studentMarks);
+//   const grade = getGrade(averageMarks);
+  
+//   console.log("Average Marks:", averageMarks);
+//   console.log("Grade:", grade);
+  
 
 
 
